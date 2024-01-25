9fbfbc729cd5ef532862852a562937acc13f278e
AlessandroSemeraro <alesemeraro1994@gmail.com>
2024-01-25 20:37:28 +0100

    added array movieList

--- classes/Movie.php
+++ classes/Movie.php
@@ -1,11 +1,40 @@
 <?php 
-
+/**
+ * this class generates a Movie
+ */
 class Movie {
-    public $name;
-    public $description;
+    public $title;
+    public $plot;
     public $ticket_price;
+    public $image;
+
+    /**
+     * Construct as a new Movie
+     *
+     * @param string $title  title film
+     * @param string $plot film description
+     * @param float $ticket_price  price ticket
+     * @param [type] $image
+     */
+    public function __construct(string $title , string $plot, float $ticket_price, $image) {
+        $this -> title= $title;
+        $this -> plot = $plot;
+        $this-> ticket_price = $ticket_price;
+        $this-> image = $image;
+    }
+
+    public function getLenghtplot(){
+        return strlen ($this -> plot);
+    }
+
+    public function getPrice(){
+        return ceil ($this -> ticket_price);
+    }
+
+
+    
 }
 
-$newMovie = new Movie();
-var_dump($newMovie)
+
+
 ?>
--- index.php
+++ index.php
@@ -1,53 +1,45 @@
+<?php 
+include_once __DIR__ . '/classes/Movie.php';
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--CSS-->
+    <link rel="stylesheet" href="style.css">
     <title>php-oop-1</title>
 </head>
 <body>
+<header>
+    <img src="" alt="">
+</header>
 
-<?php 
+<?php
 
-class Movie {
-    public $name;
-    public $description;
-    public $ticket_price;
-    public $image;
-
-    public function getLenghtDescription(){
-        return strlen ($this -> description);
-    }
-
-    public function getPrice(){
-        return ceil($this -> ticket_price);
-    }
-}
-
-//creo 2 istanze dello stesso oggetto //
-$firstMovie = new Movie();
-$secondMovie = new Movie();
-
-
-$firstMovie->name = "Harry Potter and the Philosopher's Stone";
-$firstMovie->description = "Late one night, Albus Dumbledore and Minerva McGonagall, professors at Hogwarts 
+$firstMovie = new Movie("Harry Potter and the Philosopher's Stone","Late one night, Albus Dumbledore and Minerva McGonagall, professors at Hogwarts 
 School of Witchcraft and Wizardry, along with groundskeeper Rubeus Hagrid, 
 deliver an orphaned infant wizard named Harry Potter to his Muggle aunt and uncle,
-Petunia and Vernon Dursley, his only living relatives... ";
-$firstMovie->ticket_price = 15.55 ;
+Petunia and Vernon Dursley, his only living relatives... ", 15.55, 'https://filmalibri.it/wp-content/uploads/2023/02/harry-potter-1.jpeg');
 
 
-$secondMovie->name = "The Lord of the Rings: The Fellowship of the Ring";
-$secondMovie->description = "In the Second Age of Middle-earth, the lords of Elves, Dwarves, and Men are given Rings of Power.
- Unbeknownst to them, the Dark Lord Sauron forges the One Ring in Mount Doom, 
+$secondMovie = new Movie("The Lord of the Rings: The Fellowship of the Ring","In the Second Age of Middle-earth, the lords of Elves, Dwarves, and Men are given Rings of Power.
+Unbeknownst to them, the Dark Lord Sauron forges the One Ring in Mount Doom, 
 instilling into it a great part of his power, to dominate the other Rings and conquer Middle-earth.
- A final alliance of Men and Elves battles Sauron's forces in Mordor.";
-$secondMovie->ticket_price = 29.99 ;
+A final alliance of Men and Elves battles Sauron's forces in Mordor.", 29.99, 'https://www.sideshow.com/cdn-cgi/image/height=850,quality=90,f=auto/https://www.sideshow.com/storage/product-images/502113U/the-lord-of-the-rings-the-fellowship-of-the-ring_the-lord-of-the-rings_feature.jpg');
 
 
-var_dump($firstMovie,$secondMovie);
-var_dump($firstMovie->getPrice(),$firstMovie->getLenghtDescription());
-var_dump($secondMovie->getPrice(),$secondMovie->getLenghtDescription())
+$thirdMovie = new Movie("The Prestige","In 1890s London, Robert Angier and Alfred Borden work as shills for a magician, under the mentorship of John Cutter, 
+an engineer who designs stage magic. During a water tank trick, Angier's 
+wife Julia fails to escape and drowns. Angier, devastated, accuses Borden of causing her death. 
+The two become bitter enemies and part ways.", 17.20,"https://upload.wikimedia.org/wikipedia/en/d/d2/Prestige_poster.jpg");
+
+$listMovies = [$firstMovie,$secondMovie,$thirdMovie,$firstMovie,$secondMovie,$thirdMovie];
+
+var_dump($firstMovie,$secondMovie,$thirdMovie);
+
 ?>
 
     
